03f341b02056c5aba865104275951aede9682b09
Alfredo Encina <citywideinmobi@gmail.com>
2023-03-05 01:48:09 +0000

    Update 01.js and 02.js

--- 2023-03-04-Alff2027-main/02.js
+++ 2023-03-04-Alff2027-main/02.js
@@ -3,36 +3,44 @@
   No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
-*/
+*/ 
+
+let usuarios = {
+         toni: {
+             edad: 33,
+           online: true
+        },
+         emi: {
+             edad: 25,
+           online: true
+        },
+          fran: {
+          edad: 25,
+         online: false
+       },
+        agus: {
+             edad: 24,
+           online: false
+        }
+     }; 
+     
 function cuantosOnline(usuarios) {
-  // La funcion llamada "cuantosOnline" recibe como argumento un objeto 'usuarios', cada property de ese objeto es un objeto
-  // cada usuario tiene una property 'online' que es un booleano.
-  // deberia devolver la cantidad de usuarios con la property online igual a true.
-  // Por ej:
-  // let usuarios = {
-  //     toni: {
-  //         edad: 33,
-  //         online: true
-  //     },
-  //     emi: {
-  //         edad: 25,
-  //         online: true
-  //     },
-  //     fran: {
-  //         edad: 25,
-  //         online: false
-  //     },
-  //     agus: {
-  //         edad: 24,
-  //         online: false
-  //     }
-  // }; 
-  // cuantosOnline(usuarios) devuelve 2
-  // Tip: Podes usar el metodo for...in
-
-  // Tu código aca:
-
+  
+// Tu código aca:
+  var verdaderos =0, falsos = 0;
+  
+    for (let user in usuarios){
+      console.log(user)
+      usuarios[user].online === true ? verdaderos++ : falsos++;
+    }
+    return verdaderos;
 }
+
+console.log(` Hay ${cuantosOnline(usuarios)} usuarios online`)
+document.write(` Hay ${cuantosOnline(usuarios)} usuarios online`)  
+
+  
+  
 
 
 // No modifiques nada debajo de esta linea //
